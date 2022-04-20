42321af4e17030e8cc648b79231fe6f721ca139a
anamerasul <anamerasul@gmail.com>
2022-04-21 03:28:29 +0600

    Create mongodb atlas account and Connect to database

--- node-mongo-card/index.js
+++ node-mongo-card/index.js
@@ -1,11 +1,24 @@
 import express from "express";
 
+import { MongoClient, ServerApiVersion } from "mongodb"
+
 import cors from "cors";
 
 const app = express()
 
 
 const port = process.env.PORT || 5000;
+
+const uri = "mongodb+srv://ANAMERASUL007:ANAMERASUL007@cluster0.8t0mz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+
+    console.log("db connected")
+    client.close();
+
+});
 
 app.use(cors())
 
@@ -19,3 +32,4 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
     console.log("Listening to port", port)
 })
+
